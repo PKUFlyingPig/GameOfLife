86371c79c6580e34436604c1c8e59e9671de2123
FlyingPig <1800012735@pku.edu.cn>
2020-12-04 00:05:50 +0800

    gameoflife.c finished && passed tests

--- gameoflife.c
+++ gameoflife.c
@@ -17,12 +17,64 @@
 #include <inttypes.h>
 #include "imageloader.h"
 
+int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[8] = {1, 0, -1, 1, -1, 1, 0, -1};
+
+int ring(int n, int m) {
+	return (n + m) % m;
+}
 //Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
 //Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
 	//YOUR CODE HERE
+	Color *nextState = (Color*)malloc(sizeof(Color));
+	int aliveNeighboursR = 0, aliveNeighboursG = 0, aliveNeighboursB = 0;
+	int isAliveR, isAliveG, isAliveB;
+	int idxR, idxG, idxB;
+
+	aliveNeighboursR = 0;
+	isAliveR = (*(image->image + row * image->cols + col))->R == 255;
+	isAliveG = (*(image->image + row * image->cols + col))->G == 255;
+	isAliveB = (*(image->image + row * image->cols + col))->B == 255;
+
+	for (int i = 0; i < 8; i++) {
+		int newrow = ring(row + dx[i], image->rows);
+		int newcol = ring(col + dy[i], image->cols);
+		if ((*(image->image + newrow * image->cols + newcol))->R == 255) {
+			aliveNeighboursR++;
+		}
+		if ((*(image->image + newrow * image->cols + newcol))->G == 255) {
+			aliveNeighboursG++;
+		}
+		if ((*(image->image + newrow * image->cols + newcol))->B == 255) {
+			aliveNeighboursB++;
+		}
+	}
+	idxR = 9 * isAliveR + aliveNeighboursR;
+	idxG = 9 * isAliveG + aliveNeighboursG;
+	idxB = 9 * isAliveB + aliveNeighboursB;
+
+	if (rule & (1<<idxR)) {
+		nextState->R = 255;
+	} else {
+		nextState->R = 0;
+	}
+
+	if (rule & (1<<idxG)) {
+		nextState->G = 255;
+	} else {
+		nextState->G = 0;
+	}
+
+	if (rule & (1<<idxB)) {
+		nextState->B = 255;
+	} else {
+		nextState->B = 0;
+	}
+	
+	return nextState;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
@@ -30,6 +82,17 @@ Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 Image *life(Image *image, uint32_t rule)
 {
 	//YOUR CODE HERE
+	Image *newimg = (Image *)malloc(sizeof(Image));
+	newimg->cols = image->cols;
+	newimg->rows = image->rows;
+	newimg->image = (Color**)malloc(sizeof(Color*) * (image->rows) * (image->cols));
+	Color** p = newimg->image;
+	for (int i = 0; i < newimg->rows; i++)
+		for (int j = 0; j < newimg->cols; j++) {
+			*p = evaluateOneCell(image, i, j, rule);
+			p++;
+		}
+	return newimg;
 }
 
 /*
@@ -50,4 +113,15 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if (argc != 3) {
+		printf("usage: ./gameOfLife filename rule\nfilename is an ASCII PPM file (type P3) with maximum value 255.\nrule is a hex number beginning with 0x; Life is 0x1808.");
+		return 1;
+	}
+	Image *img = readData(argv[1]);
+	uint32_t rule = strtol(argv[2], NULL, 16);
+	Image *nextImg = life(img, rule);
+	writeData(nextImg);
+	freeImage(img);
+	freeImage(nextImg);
+	return 0;
 }
